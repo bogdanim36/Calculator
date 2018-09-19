8c0d3e0c035f92be59466cda6c57da5e41e41e3b
bogdanim36 <bogdanim36@gmail.com>
2018-09-19 07:40:47 +0300

    commit

---
+++ src/components/calculator/App.History.js
@@ -0,0 +1,67 @@
+import React from 'react';
+export class AppHistory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.stack = [];
+        this.newLine();
+        this.state = { stack: [] };
+    }
+    newLine() {
+        this.stack.push([]);
+    }
+    addNumber(value) {
+        this.stack[this.stack.length - 1] += value;
+        this.setState({ stack: this.stack });
+    }
+    addOperator(value) {
+        this.stack[this.stack.length - 1] += value;
+        this.setState({ stack: this.stack });
+    }
+    addEqual(value) {
+        let line = this.stack[this.stack.length - 1];
+        if (line.length === 0)
+            return;
+        let result;
+        try {
+            result = eval(line);
+        }
+        catch (e) {
+            console.error("evaluate error for line", line);
+            return;
+        }
+        this.stack[this.stack.length - 1] += " " + value + " ";
+        this.newLine();
+        this.stack[this.stack.length - 1] = result.toString();
+        this.setState({ stack: this.stack });
+    }
+    clear() {
+        this.stack = [];
+        this.newLine();
+        this.setState({ stack: [] });
+    }
+    del() {
+        if (this.stack.length === 0)
+            return;
+        let line = this.stack[this.stack.length - 1];
+        if (line.length === 0)
+            return;
+        line = line.substring(0, line.length - 1);
+        this.stack[this.stack.length - 1] = line;
+        this.setState({ stack: this.stack });
+    }
+    render() {
+        return React.createElement("div", { className: 'history' }, this.stack.map((item, index) => {
+            return React.createElement("div", { key: index }, item);
+        }));
+    }
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+    componentWillUnmount() {
+        this.props.onRef(undefined);
+    }
+    shouldComponentUpdate() {
+        return true;
+    }
+}
+//# sourceMappingURL=App.History.js.map
